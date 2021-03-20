2b0e3b7fcbab72aa63c510b23c918ad10591e13a
Saebom An <joyan828@gmail.com>
2021-03-20 14:10:50 +0900

    Fix a spelling mistake and export bug

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,7 @@ import { applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from './logger'
 
-applyMiddleware(
+export default applyMiddleware(
     thunk,
     logger
 )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { combineReducer } from 'redux'
+import { combineReducers } from 'redux'
 import questions from './questions'
 import users from './users'
 import authedUser from './authedUser'
 
-export default combineReducer({
+export default combineReducers({
     questions, 
     users,
     authedUser
