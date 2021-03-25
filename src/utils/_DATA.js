04b841066752568488c6b1e18b4e3f5d3bfb13dd
Saebom An <joyan828@gmail.com>
2021-03-25 17:09:35 +0900

    refactor: move the api functions to _DATA.js file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,4 +1,6 @@
 import { AvatarGenerator } from 'random-avatar-generator'
+import { formatQuestion } from './formatter'
+
 const generator = new AvatarGenerator()
 
 export let users = {
@@ -123,4 +125,68 @@ export let questions = {
       }
     },
   }
-  
+ 
+
+export function _getUsers () {
+  return new Promise((res, rej) => {
+      setTimeout(() => res({...users}), 1000)
+  })
+}
+export function _getQuestions () {
+  return new Promise((res, rej) => {
+      setTimeout(() => res({...questions}), 1000)
+  })
+}
+export function _saveQuestion (question) {
+  return new Promise((res, rej) => {
+      const authedUser = question.author;
+      const formattedQuestion = formatQuestion(question);
+
+      setTimeout(() => {
+      questions = {
+          ...questions,
+          [formattedQuestion.id]: formattedQuestion
+      }
+      
+      users = {
+          ...users,
+          [authedUser]: {
+          ...users[authedUser],
+          questions: users[authedUser].questions.concat([formattedQuestion.id])
+          }
+      }
+
+      res(formattedQuestion)
+      }, 1000)
+  })
+}
+export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+  return new Promise((res, rej) => {
+      setTimeout(() => {
+          users = {
+              ...users,
+              [authedUser]: {
+                  ...users[authedUser],
+                  answers: {
+                      ...users[authedUser].answers,
+                      [qid]: answer
+                  }
+              }
+          }
+
+          questions = {
+              ...questions,
+              [qid]: {
+                  ...questions[qid],
+                  [answer]: {
+                      ...questions[qid][answer],
+                      votes: questions[qid][answer].votes.concat([authedUser])
+                  }
+              }
+          }
+
+      res({users, questions})
+      }, 500)
+  })
+}
+
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,66 +1,24 @@
-import { users, questions } from './_DATA'
-import { formatQuestion } from './formatter'
+import {  
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+} from './_DATA'
 
-export function _getUsers () {
-    return new Promise((res, rej) => {
-        setTimeout(() => res({...users}), 1000)
-    })
+export function getInitialData () {
+    return Promise.all([ 
+        _getQuestions(),
+        _getUsers()
+    ]).then(([questions, users]) => ({
+        questions, 
+        users
+    }))
 }
-export function _getQuestions () {
-    return new Promise((res, rej) => {
-        setTimeout(() => res({...questions}), 1000)
-    })
+
+export function saveQuestion (question) {
+    return _saveQuestion(question)
 }
-export function _saveQuestion (question) {
-    return new Promise((res, rej) => {
-        const authedUser = question.author;
-        const formattedQuestion = formatQuestion(question);
 
-        setTimeout(() => {
-        questions = {
-            ...questions,
-            [formattedQuestion.id]: formattedQuestion
-        }
-        
-        users = {
-            ...users,
-            [authedUser]: {
-            ...users[authedUser],
-            questions: users[authedUser].questions.concat([formattedQuestion.id])
-            }
-        }
-
-        res(formattedQuestion)
-        }, 1000)
-    })
+export function saveQuestionAnswer (info) {
+    return _saveQuestionAnswer(info)
 }
-export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-        users = {
-            ...users,
-            [authedUser]: {
-            ...users[authedUser],
-            answers: {
-                ...users[authedUser].answers,
-                [qid]: answer
-            }
-            }
-        }
-
-        questions = {
-            ...questions,
-            [qid]: {
-            ...questions[qid],
-            [answer]: {
-                ...questions[qid][answer],
-                votes: questions[qid][answer].votes.concat([authedUser])
-            }
-            }
-        }
-
-        res()
-        }, 500)
-    })
-}
-  
