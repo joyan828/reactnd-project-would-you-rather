8eb9eaaa926ac09d2ae9522a194057f7ff44eec2
Saebom An <joyan828@gmail.com>
2021-03-25 17:08:28 +0900

    refactor: Change return values of formatQuestion()

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,20 +1,21 @@
- function generateUID () {
+function generateUID () {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
-export function formatQuestion ({ optionOneText, optionTwoText, author }) {
+export function formatDate (timestamp) {
+    const d = new Date(timestamp)
+    const time = d.toLocaleTimeString('en-US')
+    return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
+
+export function formatQuestion ({ question, author }) {
+    const { timestamp, optionOne, optionTwo} = question
     return {
-        id: generateUID(),
-        timestamp: Date.now(),
         author,
-        optionOne: {
-        votes: [],
-        text: optionOneText,
-        },
-        optionTwo: {
-        votes: [],
-        text: optionTwoText,
-        }
+        id: generateUID(),
+        timestamp, 
+        optionOne,
+        optionTwo,
     }
 }
   
