e38efa2513de12634a6d2838b567ed2c5034700d
Saebom An <joyan828@gmail.com>
2021-03-28 23:47:21 +0900

    Add react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import '../styles/App.css'
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Nav from './header/Nav'
 import LoadingBar from 'react-redux-loading-bar'
 import Dashboard from './question/Dashboard'
+import QuestionPage from './question/QuestionPage'
 
 class App extends Component {
   componentDidMount() {
@@ -12,14 +15,20 @@ class App extends Component {
   
   render() {
     return (
-      <div className="App">
-        <LoadingBar />
-        { 
-          this.props.loading === true
-          ? null
-          : <Dashboard />
-        }
-      </div>
+      <Router>
+          <LoadingBar />
+          <main className="App">
+            <Nav />
+            { 
+              this.props.loading === true
+              ? null
+              : <div>
+                <Route path='/' exact component={Dashboard}/>
+                <Route path='/question/:id' component={QuestionPage}/>
+              </div>  
+            }
+          </main>
+      </Router>
     );
   }
 }
