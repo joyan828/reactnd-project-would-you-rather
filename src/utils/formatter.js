1ec069ad66b9bd6952bc475e2a2503a378a0d3d7
Saebom An <joyan828@gmail.com>
2021-03-28 23:51:46 +0900

    Change the return value of format Question()

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -5,14 +5,14 @@ function generateUID () {
 export function formatDate (timestamp) {
     const d = new Date(timestamp)
     const time = d.toLocaleTimeString('en-US')
-    return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+    return d.toLocaleDateString() + ' ' + time.substr(0, 5) + time.slice(-2)
 }
 
 export function formatQuestion ({ question, author }) {
-    const { timestamp, optionOne, optionTwo} = question
+    const { id, timestamp, optionOne, optionTwo} = question
     return {
         author,
-        id: generateUID(),
+        id,
         timestamp, 
         optionOne,
         optionTwo,
