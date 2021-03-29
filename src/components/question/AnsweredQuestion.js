680afa4616b06977da7f0da81412c3883453ed57
Saebom An <joyan828@gmail.com>
2021-03-29 22:52:57 +0900

    change question page ui

--- src/components/question/AnsweredQuestion.js
+++ src/components/question/AnsweredQuestion.js
@@ -8,29 +8,80 @@ export default function AnsweredQuestion(props) {
         return optionOne.votes.includes(props.authedUser) 
     }
 
+    const calculatePercentage = (voted, total) => {
+        return Math.round(voted/total * 100)
+    }
+
     const { 
         author, timestamp, optionOne, optionTwo
     } = props.question
+
     const totalVote = optionOne.votes.length + optionTwo.votes.length
-        
+    const optionOneRate = calculatePercentage(optionOne.votes.length, totalVote)
+    const optionTwoRate = calculatePercentage(optionTwo.votes.length, totalVote)
     return (
-      <div className="answer-card unanswered">
-        <img 
-            src= { author.avatarURL() }
-            alt= {`avatar of ${author.name}`}
-            className='avatar-img'
-        />
-        <p>asked by {author.name} </p>
-        <span>{formatDate(timestamp)}</span>
-        <p>Result: Would you rather...</p>
-        <div className={isOptionOneSelected()? 'option selected' : 'option'}>
-            <p>{optionOne.text}</p>
-            <span>{optionOne.votes.length} out of {totalVote}</span>
+      <div className='answer-card'>
+         <div className='card-info-container'>
+            <div className='questioner'>
+                <img 
+                    src= { author.avatarURL() }
+                    alt= {`avatar of ${author.name}`}
+                    className='avatar-img'
+                />
+                <p>
+                    <span className='strong'>asked by&nbsp;{author.name}</span> 
+                </p>
+            </div>
+            <div className='date-created'>{formatDate(timestamp)}</div>
         </div>
-        <div className={isOptionOneSelected()? 'option' : 'option selected'}>
-            <p>{optionTwo.text}</p>
-            <span>{optionTwo.votes.length} out of {totalVote}</span>
+        <div className='point'>
+            Result: Would you rather
+        </div> 
+        <div className='poll-result'>
+            <div className='options'>
+                <div className={isOptionOneSelected()? 'option selected' : 'option'}>
+                    <p className='option-text'>{optionOne.text}</p>
+                    <div className='graph-bar'>
+                        <span 
+                            className='bar-section' 
+                            style={{width: optionOneRate + '%'}}
+                        ></span>
+                        <span 
+                            className='percentage'
+                            style={{left: optionOneRate === 0 ? '1%' : optionOneRate -8 + '%'}}
+                        >{optionOneRate}%</span>
+                    </div>
+                    <p className='number-of-votes'>{optionOne.votes.length} out of {totalVote}</p>
+                </div>
+                <div className={isOptionOneSelected()? 'option' : 'option selected'}>
+                <p className='option-text'>{optionTwo.text}</p>
+                    <div className='graph-bar'>
+                        <span 
+                            className='bar-section' 
+                            style={{width: optionTwoRate + '%'}}
+                        ></span>
+                        <span 
+                            className='percentage'
+                            style={{left: optionTwoRate === 0 ? '1%' : optionTwoRate -8 + '%'}}
+                        >{optionTwoRate}%</span>
+                    </div>
+                    <p className='number-of-votes'>{optionTwo.votes.length} out of {totalVote}</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
       </div>
     );
 }
+
+
+{/* <div className={isOptionOneSelected()? 'option selected' : 'option'}>
+    <p>{optionOne.text}</p>
+    <span>{optionOne.votes.length} out of {totalVote}</span>
+</div>
+<div className={isOptionOneSelected()? 'option' : 'option selected'}>
+    <p>{optionTwo.text}</p>
+    <span>{optionTwo.votes.length} out of {totalVote}</span>
+</div> */}
