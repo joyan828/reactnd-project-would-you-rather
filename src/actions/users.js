4512e3e44986814aa75507fe4dafb508657ddf1c
Saebom An <joyan828@gmail.com>
2021-04-03 15:09:33 +0900

    Add user signup functionality

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,28 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
+import { saveUser } from '../utils/api'
+
+export const ADD_USER = 'ADD_USER'
 export const SAVE_ANSWER_TO_USER = 'SAVE_ANSWER_TO_USER'
 export const SAVE_QUESTION_TO_USER = 'SAVE_QUESTION_TO_USER'
+
+function addUser(user) {
+    return {
+        type: ADD_USER,
+        user
+    }
+}
+
+export function handleAddUser({id, name, password}) {
+    return (dispatch) => {
+        dispatch(showLoading())
+
+        return saveUser({id, name, password})
+            .then(({ user }) => {
+                dispatch(addUser(user))
+                dispatch(hideLoading())
+            })
+    }
+}
 
 export function saveAnswerToUser (users) {
     return {
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,22 @@
 import {RECEIVE_DATA} from '../actions/shared'
-import {SAVE_ANSWER_TO_USER, SAVE_QUESTION_TO_USER} from '../actions/users'
+import {
+    ADD_USER,
+    SAVE_ANSWER_TO_USER, 
+    SAVE_QUESTION_TO_USER
+} from '../actions/users'
 
 export default function questions (state = {}, action) {
     switch( action.type ) {
         case RECEIVE_DATA : 
             return action.users
+        case ADD_USER : 
+            const { user } = action
+            return {
+                ...state,
+                [user.id] : {
+                    ...user
+                }
+            }
         case SAVE_ANSWER_TO_USER : 
             return action.users
         case SAVE_QUESTION_TO_USER : 
